a8f21a7bdaa910474a6a16889aa073bf0844be36
priyanka.parekh <priyanka.parekh@rapidops.co>
2025-03-12 01:50:00 +0530

    navbar done

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
-import mobileLogo from "../../assets/images/mobile-logo-nav.png";
+import MobileLogo from "../../assets/images/mobilelogo";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,75 +18,111 @@ function App() {
         }`}
         onClick={() => setIsOpen(false)}
       />
-      <nav className="py-4 fixed w-full top-0 z-50 bg-[#fffae5]">
-        <div className="max-w-7xl mx-auto px-6">
-          <div className="flex justify-around items-center">
-            {/* Mobile Menu Button */}
-            <div className="md:hidden">
-              <button onClick={toggleMenu} className="text-primary-black">
-                <div className="w-6 h-6 flex items-center justify-center">
-                  <div className={`menu-icon ${isOpen ? "open" : ""}`}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </div>
+      <nav
+        className={`py-4 fixed w-full top-0 z-50 transition-all duration-300 ${
+          isOpen ? "bg-[#b1b1b1]" : "bg-[#fffae5]"
+        }`}
+      >
+        <div className="">
+          <div
+            className={`flex lg:justify-center justify-center max-w-full items-center transition-opacity duration-300 ${
+              isOpen ? "opacity-0" : "opacity-100"
+            }`}
+          >
+            <div
+              className={`${
+                window.matchMedia("(max-width: 767px)").matches
+                  ? "flex md:justify-around sm:justify-around items-center w-full"
+                  : ""
+              }`}
+            >
+              {/* Mobile Menu Button */}
+
+              <div
+                className={`${
+                  window.matchMedia("(max-width: 767px)").matches
+                    ? "flex items-center"
+                    : ""
+                }`}
+              >
+                <div className="md:hidden">
+                  <button onClick={toggleMenu} className="text-primary-black">
+                    <div className="w-6 h-6 flex items-center justify-center">
+                      <div className={`menu-icon ${isOpen ? "open" : ""}`}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </div>
+                    </div>
+                  </button>
                 </div>
+                {/* Logo */}
+                <div
+                  className={`${
+                    window.matchMedia("(max-width: 767px)").matches
+                      ? "flex items-center ms-5"
+                      : ""
+                  }`}
+                >
+                  <Link to="/" className="flex items-center">
+                    <img
+                      src={logo}
+                      className="h-16 w-full sm:h-10 md:h-16 lg:h-16"
+                      alt="logo"
+                      title="logo"
+                    />
+                  </Link>
+                </div>
+              </div>
+              <button className="md:hidden bg-primary-yellow text-primary-black px-2 py-1 rounded-md font-montserrat text-[8px] hover:bg-primary-black hover:text-primary-yellow transition-colors">
+                LET'S TALK
               </button>
             </div>
-            {/* Logo */}
-            <div className="flex items-center">
-              <Link to="/" className="flex items-center">
-                <img src={logo} className="h-16 sm:h-8 md:h-16 lg:h-16 sm:-ms-3" alt="logo" title="logo" />
-              </Link>
-            </div>
-            <button className="md:hidden bg-primary-yellow text-primary-black px-3 py-2 rounded-lg font-montserrat text-sm font-medium hover:bg-primary-black hover:text-primary-yellow transition-colors tracking-wide">
-              LET'S TALK
-            </button>
             {/* Desktop Navigation */}
-            <div className="hidden md:flex items-center space-x-16">
+            <div className="hidden md:flex items-center space-x-24 lg:space-x-24 md:space-x-2">
               <a
                 href="/"
-                className="relative font-montserrat text-sm font-medium text-primary-black hover:text-primary-black transition-colors 
+                className="relative font-montserrat text-md font-medium text-primary-black hover:text-primary-black transition-colors ms-16 lg:ms-16 md:ms-5
               before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-primary-yellow before:transition-all before:duration-300 hover:before:w-full tracking-wide"
               >
                 HOME
               </a>
               <a
                 href="/about"
-                className="relative font-montserrat text-sm font-medium text-primary-black hover:text-primary-black transition-colors 
+                className="relative font-montserrat text-md font-medium text-primary-black hover:text-primary-black transition-colors 
               before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-primary-yellow before:transition-all before:duration-300 hover:before:w-full tracking-wide"
               >
                 ABOUT
               </a>
               <a
                 href="/services"
-                className="relative font-montserrat text-sm font-medium text-primary-black hover:text-primary-black transition-colors 
+                className="relative font-montserrat text-md font-medium text-primary-black hover:text-primary-black transition-colors 
               before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-primary-yellow before:transition-all before:duration-300 hover:before:w-full tracking-wide"
               >
                 OUR SERVICES
               </a>
               <a
                 href="/corporate"
-                className="relative font-montserrat text-sm font-medium text-primary-black hover:text-primary-black transition-colors 
+                className="relative font-montserrat text-md font-medium text-primary-black hover:text-primary-black transition-colors 
               before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-primary-yellow before:transition-all before:duration-300 hover:before:w-full tracking-wide"
               >
                 CORPORATE
               </a>
               <a
                 href="/merch"
-                className="relative font-montserrat text-sm font-medium text-primary-black hover:text-primary-black transition-colors 
+                className="relative font-montserrat text-md font-medium text-primary-black hover:text-primary-black transition-colors 
               before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-primary-yellow before:transition-all before:duration-300 hover:before:w-full tracking-wide"
               >
                 MENDING MERCH
               </a>
               <a
                 href="/resources"
-                className="relative font-montserrat text-sm font-medium text-primary-black hover:text-primary-black transition-colors 
+                className="relative font-montserrat text-md font-medium text-primary-black hover:text-primary-black transition-colors 
               before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-primary-yellow before:transition-all before:duration-300 hover:before:w-full tracking-wide"
               >
                 RESOURCES
               </a>
-              <button className="bg-primary-yellow text-primary-black px-6 py-2 rounded-lg font-montserrat text-sm font-medium hover:bg-primary-black hover:text-primary-yellow transition-colors tracking-wide">
+              <button className="bg-primary-yellow text-primary-black px-6 py-3 rounded-lg font-montserrat text-md font-medium hover:bg-primary-black hover:text-primary-yellow transition-colors tracking-wide">
                 LET'S TALK
               </button>
             </div>
@@ -99,12 +135,10 @@ function App() {
             }`}
           >
             <div className="bg-[#efc345] rounded-br-[40px] p-8">
-              <div className="flex flex-col space-y-6">
-                <img
-                  src={mobileLogo}
-                  alt="mobile-logo"
-                  className="h-16 w-full"
-                />
+              <div className="flex flex-col space-y-4 pl-8">
+                <div>
+                  <MobileLogo />
+                </div>
                 <a
                   href="/"
                   className="font-montserrat text-md font-medium text-black tracking-wide"
@@ -171,7 +205,7 @@ function App() {
                       d="m5.5 5.3c0 0.6 0.5 1.1 1.2 1.1 0.6 0 1.1-0.5 1.1-1.1 0-0.6-0.5-1.1-1.1-1.1-0.7 0-1.2 0.5-1.2 1.1z"
                     />
                   </svg>
-                  <button className="bg-white text-primary-black px-4 py-4 rounded-lg font-montserrat text-md font-bold flex items-center tracking-wide">
+                  <button className="mb-10 bg-white text-primary-black px-4 py-3 rounded-lg font-montserrat text-md font-bold flex items-center tracking-wide">
                     LET'S TALK
                   </button>
                 </div>
